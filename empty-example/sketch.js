f932168c23470d12d38a11a24d934107cf3ec8e0
N8orious <keshavraj6301@gmail.com>
2020-03-13 23:33:07 +0530

    first

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,86 +1,76 @@
-var ship;
-var enemy = [];
-var bullets = [];
-
-class Enemy
-{
-constructor(_pos, _vel)
-  {
-    this.vel = _vel;
-    this.pos = _pos;
-}
-  show() {
- 
-    circle( this.pos.x, this.pos.y, 100);
-         }
-
-  move() {
-   
- }
-
-  death() {
-  
-}
-  
-  bulletcollision() {
-    
-  }
-  
-  class Bullet{
-  constructor(_pos, _vel) {
-    this.pos = _pos;
-    this.vel = _vel;
-    this.timer = millis();
-    this.radius = 3;
-  }
-  show() {
-    circle(this.pos.x, this.pos.y, this.radius * 2);
-  }
-  move() {
-    this.pos.add(this.vel);
-  }
-  death() {
-    if (millis() - this.timer > 5000) {
-      bullets.shift();
-    }
-  }
-  
-  class Ship {
-  constructor(_pos, _vel,) {
-    this.pos = _pos;
-    this.vel = _vel;
-  }
-  show() {
-    stroke(255);
-    strokeWeight(3);
-    let x1 = this.pos.x;
-    let y1 = this.pos.y;
-    let x2 = this.pos.x + 5 * this.vel.copy().normalize().x;
-    let y2 = this.pos.y + 5 * this.vel.copy().normalize().y;
-    line(x1, y1, x2, y2);
-  }
-  
-  
-  }
-
-
-
-
+let w;
+let column;
+let row;
+let board;
+let next;
 
 
 function setup() {
   createCanvas(400, 400);
+  w = 25;
+  column = floor(width / w);;
+  row = floor(height / w);
+  //creating 2d array
+  board = new Array(column);
+  for (let i = 0; i < column; i++) {
+    board[i] = new Array(row);
+  }
+  //for the swap
+  next = new Array(column);
+  for (i = 0; i < column; i++) {
+    next[i] = new Array(row);
+}
+init();
 }
 
+
 function draw() {
-  background(1);
-  
-  let x = width/2;
-  let y = height/2;
-  
-  
-}
-    function KeyPressed() {
-      // left arrow and right arrow for ship
-      // space for bullets
+  background(255);
+  generate(); //calling the new generation fn.
+  for (let i = 0; i < column; i++) {
+    for (let j = 0; j < row; j++) {
+      if ((board[i][j] == 1)) fill(0);
+      else fill(255);
+      stroke(0);
+      rect(i * w, j * w, w - 1, w - 1);
     }
+  }
+}
+
+function mousePressed() {
+  init();
+}
+
+function init() {
+
+  for (let i = 0; i < column; i++) {
+    for (let j = 0; j < row; j++) {
+      board[i][j] = floor(random(2));
+      next[i][j] = 0
+    }
+  }
+}
+
+function generate() {
+  for (let x = 1; x < column-1; x++) {
+    for (let y = 1; y < row-1; y++) {
+
+      let neighbors = 0;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          neighbors += board[x + i][y + j];
+        }
+      }
+
+      neighbors -= board[x][y];
+      // Rules of Life
+      if ((board[x][y] == 1) && (neighbors < 2)) next[x][y] = 0; // Loneliness
+      else if ((board[x][y] == 1) && (neighbors > 3)) next[x][y] = 0; // Overpopulation
+      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1; // Reproduction
+      else next[x][y] = board[x][y]; // Stasis
+    }
+  }
+let temp = board;
+  board = next;
+  next = temp;
+}
